441210841fb72a00f9664494abb703e1748d6601
lucas-s0usa <lucassousadasilva2007@gmail.com>
2026-04-01 13:36:36 -0300

    Classe Funcionario

---
+++ eclipse-workspace/petshop/src/petshop/Funcionario.java
@@ -0,0 +1,26 @@
+package petshop;
+
+public class Funcionario extends Pessoa {
+    
+    private String cargo;
+
+    public Funcionario(String nome, String cpf, String genero, String cargo) {
+        super(nome, cpf, genero); 
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public void mostrarInformacoes() {
+    System.out.println("Nome: " + getNome());
+    System.out.println("Cpf: " + getCpf());
+    System.out.println("Gênero: " + getGenero());
+    System.out.println("Cargo: " + this.cargo);
+    }
+}
